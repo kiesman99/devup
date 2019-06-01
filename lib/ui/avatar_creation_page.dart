a04ac25ec245a7907162be913c5064ca8f3edf3c
Justin <justin.vietz@gmx.de>
2019-06-01 15:49:37 +0200

    Auto stash before merge of "master" and "origin/master"

--- lib/ui/avatar_creation_page.dart
+++ lib/ui/avatar_creation_page.dart
@@ -139,29 +139,6 @@ class _AvatarCreatorPageState extends State<AvatarCreatorPage> {
   }
 }
 
-class SVGHolder extends StatelessWidget {
-
-  final Widget child;
-
-  final double marginTop;
-  final double marginBottom;
-
-  SVGHolder({this.marginTop = 0, this.marginBottom = 0, this.child});
-
-  SVGHolder.symmetrical({double margin}) : this(marginTop: margin, marginBottom: margin);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        top: marginTop,
-        bottom: marginBottom
-      ),
-      child: child,
-    );
-  }
-}
-
 
 /// Just a simple placeholder
 /// SVG PLACEHOLDER
