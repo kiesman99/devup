e847ce7f64d80ee4a9caf6f62cdb155a8aef5ecb
Justin <justin.vietz@gmx.de>
2019-06-07 01:02:06 +0200

    Added Mock SwipeCard
    Data provided by Firebase is currently not usable

--- lib/widgets/swipe_card.dart
+++ lib/widgets/swipe_card.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:devup/backend.dart';
 import 'package:devup/model/user.dart';
 import 'package:flutter/material.dart';
@@ -8,17 +9,50 @@ class SwipingCard extends StatelessWidget {
   final Size screenSize;
   final Function swipeLeft, swipeRight;
   final DecorationImage image;
+  final bool hasElevation;
 
-  SwipingCard({this.user, this.screenSize, this.image, this.swipeLeft, this.swipeRight});
+  SwipingCard({this.user, this.screenSize, this.image, this.swipeLeft, this.swipeRight, this.hasElevation});
+
+  SwipingCard.mocked({
+    Size screenSize,
+    DecorationImage image,
+    Function swipeLeft,
+    Function swipeRight,
+    bool hasElevation
+  }) : this(
+    screenSize: screenSize,
+    image: image,
+    swipeLeft: swipeLeft,
+    swipeRight: swipeRight,
+    hasElevation: hasElevation,
+    user: User.fromJson(
+        {
+          "userName": "Username",
+          "hair"
+          "eyes"
+          "nose": "Nose1",
+          "mouth": "Mouth1",
+          "chin": "Beard1",
+          "spokenLanguages": [
+            "German",
+            "Spanish"
+          ],
+          "location": GeoPoint(17.0, 18.0),
+          "age": 27,
+          "programmingLanguages": ["JS", "CSS"],
+          "experience": "0"
+        }
+    )
+  );
 
   @override
   Widget build(BuildContext context) {
     final String experience = backend<ResourceService>().experience[user.experience];
 
+
     return Card(
-      color: Colors.green.withOpacity(0.3),
-      elevation: 4.0,
-      child: Container(                      
+      elevation: hasElevation ? 4.0 : 0.0,
+      child: Container(
         width: screenSize.width / 1.2,
         height: screenSize.height / 1.3,
         decoration: new BoxDecoration(
